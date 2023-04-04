22d6160b73885e2c859c27ff65f020f8b94f3e35
Victor Ivanov <vi.ivanov@vk.team>
2023-04-05 01:48:38 +0300

    add new styles for button component

--- packages/paaskit/src/components/button/button.tsx
+++ packages/paaskit/src/components/button/button.tsx
@@ -4,11 +4,17 @@ import style from './button.module.css';
 type Props = {
     children: React.ReactNode,
 	callback?: (data: any) => void,
+	type?: 'input',
+	isSec?: boolean,
 };
 
-const MyButton: React.FC<Props> = ({ children, callback }) => {
+const MyButton: React.FC<Props> = ({ children, callback, type, isSec = false }) => {
 	return (
-		<div onClick={callback} className={style.block}>
+		<div onClick={callback} className={
+			style.block
+			+ type ? (' ' + style[type ?? 'block']) : ''
+			+ isSec ? (' ' + style.sec) : ''
+		}>
 			{children}
 		</div>
 	);
