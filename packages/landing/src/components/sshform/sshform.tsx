aa0676644e87b890260aa75307778c3f1526ad10
Victor Ivanov <vi.ivanov@vk.team>
2023-04-15 12:38:45 +0300

    add succes notify and msg

--- packages/landing/src/components/sshform/sshform.tsx
+++ packages/landing/src/components/sshform/sshform.tsx
@@ -5,6 +5,8 @@ import { Store } from 'react-notifications-component';
 
 
 const SSHForm: React.FunctionComponent = () => {
+    const [isDeploed, setDeploy] = React.useState(false);
+
     // @ts-ignore
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -17,14 +19,29 @@ const SSHForm: React.FunctionComponent = () => {
             password: event.target.elements.password.value,
         };
         try {
-            await fetch('http://localhost:8090/deploy-app', {
+            await fetch('http://89.208.220.55:8091/deploy-app', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(data),
             });
-        } catch {};
+        } catch {
+            Store.addNotification({
+                title: "Ошибка!",
+                message: "Проверьте корректность введенных данных",
+                type: "danger",
+                insert: "top",
+                container: "top-right",
+                animationIn: ["animate__animated", "animate__fadeIn"],
+                animationOut: ["animate__animated", "animate__fadeOut"],
+                dismiss: {
+                  duration: 5000,
+                  onScreen: true
+                }
+            });
+            return;
+        };
 
         Store.addNotification({
             title: "Успех!",
@@ -39,6 +56,8 @@ const SSHForm: React.FunctionComponent = () => {
               onScreen: true
             }
         });
+
+        setDeploy(data.ip);
     };
 
     return (
@@ -46,16 +65,23 @@ const SSHForm: React.FunctionComponent = () => {
             <Text type='h2'>Введите данные для подключения по SSH к Вашему серверу и разверните нашу систему</Text>
             <Text type='pDesc'>Наши сервера в автоматическом режиме подключатся по SSH к Вашему серверу и установят последнюю версию нашего Control-plane, предоставив Вам готовый к использованию сервис</Text>
 
-            <form className={style.form} onSubmit={onSubmit}>
-                <input type='text' name='ip' placeholder='IP адресс'></input>
-                <input type='text' name='port' placeholder='Port'></input>
-                <input type='text' name='user' placeholder='Пользователь'></input>
-                <input type='password' name='password' placeholder='Пароль'></input>
-                <input type='submit' className={style.submit} id={'pzd-reshenie'}></input>
-                <Button callback={() => document.getElementById('pzd-reshenie')?.click()}>
-                    Развернуть систему
-                </Button>
-            </form>
+            {isDeploed ?
+                <form className={style.form} onSubmit={onSubmit}>
+                    <input type='text' name='ip' placeholder='IP адресс'></input>
+                    <input type='text' name='port' placeholder='Port'></input>
+                    <input type='text' name='user' placeholder='Пользователь'></input>
+                    <input type='password' name='password' placeholder='Пароль'></input>
+                    <input type='submit' className={style.submit} id={'pzd-reshenie'}></input>
+                    <Button callback={() => document.getElementById('pzd-reshenie')?.click()}>
+                        Развернуть систему
+                    </Button>
+                </form>
+            :
+                <Text type='pDesc'>
+                    <br/><br/><br/><br/><br/>
+                    Приложение успешно <b>развернуто</b>.<br/> Перейдите по ссылке <a href={'http://' + isDeploed + '/'}>{'http://' + isDeploed + '/'}</a>, чтобы открыть админ панель.
+                </Text>
+            }
         </div>
     );
 };
