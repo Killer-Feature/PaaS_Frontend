d76ccfdda7b20a8f6b7db47ab0b874dff1cdc569
Victor Ivanov <vi.ivanov@vk.team>
2023-04-17 20:17:06 +0300

    add warning modal if cluster didnt exist

--- packages/adminpanel/src/views/index/index.tsx
+++ packages/adminpanel/src/views/index/index.tsx
@@ -6,6 +6,7 @@ import viteLogo from '/vite.svg'
 import { Text, Header, Button } from 'paaskit';
 import Table from '../../components/table/nodes/table';
 import ModalState, {Modals} from '../../models/modal';
+import NodesStore from '../../models/nodes';
 
 const Index = () => {
     return (
@@ -21,7 +22,16 @@ const Index = () => {
                     Состояние кластера
                 </Title>
                 <div className={style.buttons}>
-                    <Button isSec callback={() => ModalState.open(Modals.Config)}>
+                    <Button isSec callback={() => {
+                        if (NodesStore.isHasCluster) {
+                            ModalState.open(Modals.Config);
+                        } else {
+                            ModalState.open(Modals.Warning, {
+                                title: 'Нельзя посмотреть конфиг',
+                                description: 'Вы не подключили ни одну тачку к кластеру, поэтому кластер не создан и открыть конфиг невозможно'
+                            });
+                        }
+                    }}>
                         <span>Открыть конфиг</span>
                     </Button>
                     <Button callback={() => ModalState.open(Modals.CreateNode)}>
